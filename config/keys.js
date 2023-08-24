4bc97514141ac65ab182ab1487698de987f2f144
AwesomeDev777 <bnykyfor@gmail.com>
2023-08-25 01:12:20 +0300

    mongod uri

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secretOrKey':'secret',
   // 'mongoURI': 'mongodb://localhost:27017/server',
-  'mongoURI': 'mongodb://admin:PJDbPiigH5ECBxom@SG-river-lemur-3970-59487.servers.mongodirector.com:27017/test'
+  'mongoURI': 'mongodb://admin:PJDbPiigH5ECBxom@SG-river-lemur-3970-59487.servers.mongodirector.com:27017/admin'
 };
